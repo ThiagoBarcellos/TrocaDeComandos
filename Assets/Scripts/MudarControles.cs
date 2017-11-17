e29fb285fd9d152bccda4ee551d42cb43ed97f02
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2017-11-17 10:32:44 -0200

    Gerenciador de Teclas
    Script para Troca de teclas pela interface do unity

---
+++ Assets/Scripts/MudarControles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudarControles : MonoBehaviour {
+
+	//public KeyCode[] controllers = {KeyCode.A, KeyCode.D, KeyCode.W, KeyCode.S, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow};
+
+	public KeyCode jumpKey;
+	public KeyCode leftKey;
+	public KeyCode rightKey;
+	public KeyCode downKey;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public bool IsJumpDown(){
+		return Input.GetKeyDown (jumpKey);
+	}
+	public bool IsCrouching(){
+		return Input.GetKey (downKey);
+	}
+	public bool IsMovingLeft(){
+		return Input.GetKey (leftKey);
+	}
+	public bool IsMovingRight(){
+		return Input.GetKey (rightKey);
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public sealed class PlayerJump : MonoBehaviour
+{
+    public PlayerController controller;
+	public MudarControles mc;
+
+    public float jumpImpulse = 10.0f;
+
+    private void Update()
+    {
+		if (controller.isGrounded && !controller.isStunned && mc.IsJumpDown())
+            controller.playerRigidbody.AddForce(new Vector2(0.0f, jumpImpulse), ForceMode2D.Impulse);
+    }
+}
